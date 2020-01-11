3537fdc71dd1f3f905aed13c4d509249aec43d11
natedevlinux <natedevlinux@gmail.com>
2020-01-11 07:03:31 -0700

    Updates

---
+++ precache-manifest.10178a948563fe7950b323c1b2194fa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23cbb65d46750aa3843035f0e8b1454",
+    "url": "/todolistapp/index.html"
+  },
+  {
+    "revision": "1cb0320b99cffa2757cb",
+    "url": "/todolistapp/static/css/main.373a1d18.chunk.css"
+  },
+  {
+    "revision": "f24db0997ef935413307",
+    "url": "/todolistapp/static/js/2.54f74e14.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolistapp/static/js/2.54f74e14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cb0320b99cffa2757cb",
+    "url": "/todolistapp/static/js/main.81a106a4.chunk.js"
+  },
+  {
+    "revision": "189ad49f528b374e7f5c",
+    "url": "/todolistapp/static/js/runtime-main.d63f0861.js"
+  },
+  {
+    "revision": "b502861b69adb2a1cfd474f406680339",
+    "url": "/todolistapp/static/media/bg-1.b502861b.jpeg"
+  }
+]);
